480bd42d549d5f8732f42f1b5e5daa9cfd5e616a
latte-y <yukimkyn1119@gmail.com>
2024-04-02 08:56:51 +0900

    shojin

---
+++ A.cpp
@@ -0,0 +1,67 @@
+/* Blood Pressure */
+#include <iostream>
+
+int main() {
+    double A, B;
+    std::cin >> A >> B;
+    std::cout << (A-B)/3 + B << std::endl;
+
+    return 0;
+}
+
+
+/* Rotate */
+// #include <iostream>
+
+// int main() {
+//     std::string S, S_dash;
+//     std::cin >> S;
+//     std::cout << S[1] << S[2] << S[0] << std::endl;
+
+//     return 0;
+// }
+
+
+/* Tires */
+// #include <iostream>
+
+// int main() {
+//     std::string S;
+//     std::cin >> S;
+//     if (S[S.length()-1] == 'r') std::cout << "er" << std::endl;
+//     else std::cout << "ist" << std::endl;
+
+//     return 0;
+// }
+
+
+/* Chinchirorin*/
+// #include <iostream>
+
+// int main() {
+//     int a,b,c;
+//     std::cin >> a >> b >> c;
+
+//     if (a==b)      std::cout << c << std::endl;
+//     else if (b==c) std::cout << a << std::endl;
+//     else if (c==a) std::cout << b << std::endl;
+//     else std::cout << 0 << std::endl;
+
+//     return 0;
+// }
+
+
+/* Long Loong*/
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int N;
+//     cin >> N;
+
+//     cout << 'L';
+//     for (int i=0; i<N; i++) cout << 'o';
+//     cout << "ng" << endl;
+
+//     return 0;
+// }
